50b7e6ca4d438ff72ce7bac088cd4259eeb2ca42
amolshinde7185 <amolshinde7185@gmail.com>
2026-04-15 21:57:00 +0530

    Login page changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
 const {
   addCucumberPreprocessorPlugin,
 } = require("@badeball/cypress-cucumber-preprocessor");
 
-const createEsbuildPlugin =
-  require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/features/*.feature",
+    specPattern: "cypress/e2e/**/*.feature",
 
     async setupNodeEvents(on, config) {
       await addCucumberPreprocessorPlugin(on, config);
@@ -17,7 +19,7 @@ module.exports = defineConfig({
       on(
         "file:preprocessor",
         createBundler({
-          plugins: [createEsbuildPlugin.default(config)],
+          plugins: [createEsbuildPlugin(config)],
         })
       );
 
